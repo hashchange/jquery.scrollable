f4e9508bd56518b76c2d50c347d788df99175423
hashchange <heim@zeilenwechsel.de>
2015-06-01 18:41:25 +0200

    Updated build

--- dist/jquery.scrollable.js
+++ dist/jquery.scrollable.js
@@ -1,4 +1,4 @@
-// jQuery.scrollable, v0.1.2
+// jQuery.scrollable, v0.2.0
 // Copyright (c)2015 Michael Heim, Zeilenwechsel.de
 // Distributed under MIT license
 // http://github.com/hashchange/jquery.scrollable
@@ -939,14 +939,16 @@
     
     } )( lib );
     /**
-     * Core, option B:
+     * Core, option A:
      *
-     * The scrollable element for a window is not detected. Instead, both possible candidates (document Element, body) are
-     * animated.
+     * The scrollable element for a window is detected with a feature test.
      */
     
     ( function ( core, lib ) {
         "use strict";
+    
+        /** @type {string}  caches the name of the element to use for window scrolling. Values: "body" or "html" */
+        var _windowScrollable;
     
         /**
          * Returns the scrollable element, in a jQuery wrapper. The container element is expected to be normalized.
@@ -955,17 +957,17 @@
          *
          * - For ordinary elements, it returns the element itself.
          *
-         * - For the window, it returns a set consisting of body and documentElement. Document, documentElement and body are
-         *   considered to mean "window", and are treated the same.
+         * - For the window, it returns either body or documentElement, depending on the browser whether or not it is in
+         *   quirks mode. Document, documentElement and body are considered to mean "window", and are treated the same.
          *
-         * - For an iframe element, it returns body and documentElement of the iframe content window.
+         * - For an iframe element, it returns the scrollable element of the iframe content window.
          *
          * If an empty jQuery set is passed in, an empty jQuery set is returned.
          *
          * @param   {jQuery} $container
          * @returns {jQuery}
          */
-        core.getScrollable = function ( $container ) {
+         core.getScrollable = function( $container ) {
             return $.isWindow( $container[0] ) ? getWindowScrollable( $container[0] ) : $container;
         };
     
@@ -979,89 +981,72 @@
         core.animateScroll = function ( $container, position, options ) {
     
             var $scrollable = core.getScrollable( $container );
-    
-            if ( $.isWindow( $container[0] ) ) {
-                animateWindowScroll( $scrollable, position, options );
-            } else {
-                lib.addScrollAnimation( $scrollable, position, options );
-            }
-    
+            lib.addScrollAnimation( $scrollable, position, options );
         };
     
+        /**
+         * Core internals
+         */
     
-        function animateWindowScroll ( $scrollable, position, options ) {
-    
-            var callbacks = lib.extractCallbacks( options ),
-                calledInBodyContext = function () {
-                    var elem = this;
-                    return elem && elem.nodeName && elem.nodeName.toLowerCase() === "body";
-                };
-    
-            // Here, we are mostly concerned with preventing callbacks from firing twice.
-            //
-            // We simply filter out the calls for one animated element and let them pass for the other. Which element? That
-            // is an arbitrary choice. We cannot know the true scrollable element, so we just pick one. We go with "body"
-            // here.
-            //
-            // Luckily for us, the choice of element doesn't matter. The callbacks are fired an identical number of times
-            // for both elements, and they are passed the exact same data for each call, even though one of the elements
-            // isn't animated at all.
-            $.each( callbacks, function ( name, callback ) {
-                options[name] = runIf( callback, calledInBodyContext );
-            } );
-    
-            // Run the animation.
-            lib.addScrollAnimation( $scrollable, position, options );
-    
-        }
-    
+        /**
+         * Detects which element to use for scrolling a window (body or documentElement). Returns the element, in a jQuery
+         * wrapper.
+         *
+         * The detection is sandboxed in an iframe created for the purpose.
+         *
+         * @param {Window} [currentWindow=window]  needed to detect quirks mode in a particular window, and to return the
+         *                                         scrollable element in the right window context
+         * @returns {jQuery}
+         */
         function getWindowScrollable ( currentWindow ) {
-            return $( [ currentWindow.document.documentElement, currentWindow.document.body ] );
+            var iframe,
+                currentDocument = ( currentWindow || window ).document;
+    
+            // In quirks mode, we have to scroll the body, regardless of the normal browser behaviour
+            if ( currentDocument.compatMode === "BackCompat" ) return $( currentDocument.body );
+    
+            if ( ! _windowScrollable ) {
+                iframe = createScrollableTestIframe();
+    
+                iframe.contentWindow.scrollTo( 1, 0 );
+                _windowScrollable = iframe.contentDocument.documentElement.scrollLeft === 1 ? "html" : "body";
+    
+                document.body.removeChild( iframe );
+            }
+    
+            return _windowScrollable === "html" ? $( currentDocument.documentElement ) : $( currentDocument.body );
         }
     
         /**
-         * Takes a function and returns a function which skips every other call. Even calls (2nd, 4th) are executed, while
-         * the odd ones (1st call, 3rd etc) are dropped.
+         * Creates an iframe document with an HTML5 doctype and UTF-8 encoding and positions it off screen. Window size
+         * is 500px x 500px. Its body is 550px wide, in preparation for a horizontal scroll test. Returns the iframe element.
          *
-         * The original function, when actually called, is passed all the arguments. It is executed in the global context
-         * (window) unless it has been bound to a specific context prior to being passed in.
+         * The iframe content has to overflow horizontally, not vertically, because of an iOS quirk. Iframes expand
+         * vertically in iOS until the content fits inside (and as a result, we are unable to scroll vertically). But
+         * iframes don't expand horizontally, so scrolling on that axis works in iOS.
          *
-         * @param {Function} fn
+         * @returns {HTMLIFrameElement}
          */
-        function skipOddCalls( fn ) {
+        function createScrollableTestIframe () {
+            var iframe = document.createElement( "iframe" ),
+                body = document.body;
     
-            return (function ( count ) {
+            iframe.style.cssText = "position: absolute; top: -600px; left: -600px; width: 500px; height: 500px; margin: 0px; padding: 0px; border: none; display: block;";
+            iframe.frameborder = "0";
     
-                count = 0;
-                return function () {
-                    count++;
-                    if ( count % 2 === 0 ) fn.apply( undefined, arguments );
-                };
+            body.appendChild( iframe );
+            iframe.src = 'about:blank';
     
-            } )();
+            iframe.contentDocument.write( '<!DOCTYPE html><html><head><meta charset="UTF-8"><title></title><style type="text/css">body { width: 550px; height: 1px; }</style></head><body></body></html>' );
     
+            return iframe;
         }
     
-        /**
-         * Creates a wrapper function which executes a function if a test returns true.
-         *
-         * Both the target function and the test function are passed the arguments the wrapper is called with. They are also
-         * invoked with the same context the wrapper is set to (window by default).
-         *
-         * If either the target function or the test function need a specific context which is different from the one in
-         * runIf, they must be bound to that context before being passed in.
-         *
-         * @param   {Function} fn
-         * @param   {Function} test
-         * @returns {Function}
-         */
-        function runIf ( fn, test ) {
-    
-            return function () {
-                if ( test.apply( this, arguments ) ) fn.apply( this, arguments );
-            };
-    
-        }
+        // Let's prime getWindowScrollable() immediately after the DOM is ready. It is best to do it up front because the
+        // test touches the DOM, so let's get it over with before people set up handlers for mutation events and such.
+        $( function () {
+            if ( _windowScrollable === undefined ) getWindowScrollable();
+        } );
     
     } )( core, lib );
     
